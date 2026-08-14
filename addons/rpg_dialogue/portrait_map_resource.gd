902c0f9f0457a2d80727380a5fd29f28e6747c49
diltdicker <diltdicker@gmail.com>
2026-08-13 20:10:50 -0700

    audio + portrait resource types (#28)

--- addons/rpg_dialogue/portrait_map_resource.gd
+++ addons/rpg_dialogue/portrait_map_resource.gd
@@ -1,9 +1,9 @@
 @tool
 extends Resource
 
-class_name RPGDLAudioChannelResource
+class_name RPGDLPortraitMapResource
 
-const ALLOWED_TYPES : PackedStringArray = ["wav", "mp3", "ogg"]
+const ALLOWED_TYPES : PackedStringArray = ["png", "tga"]
 
 func select_folder():
 	var dialog = FileDialog.new()
@@ -15,7 +15,7 @@ func select_folder():
 			if file_name.get_extension() in ALLOWED_TYPES:
 				var key = file_name.get_basename().replace(" ", "_").to_lower()
 				var value = path.path_join(file_name)
-				sounds[key] = load(value)
+				portraits[key] = load(value)
 		
 		dialog.queue_free()
 	)
@@ -27,6 +27,6 @@ func select_folder():
 	Engine.get_main_loop().root.add_child(dialog)
 	dialog.popup_centered(Vector2(640, 480))
 
-@export_tool_button("Import Audio From Folder", 'Folder') var import = select_folder
+@export_tool_button("Import Portraits From Folder", 'Folder') var import = select_folder
 
-@export var sounds : Dictionary[String, AudioStream]
+@export var portraits : Dictionary[String, Texture2D]
